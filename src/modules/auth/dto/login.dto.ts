101309aeffa703960c20ae16aa359a5fa448f270
Ghazan Khan <ghazankhan27@hotmail.com>
2026-02-18 16:20:31 +0500

    fix: Minor fix in login dto

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,11 +1,10 @@
-import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
+import { IsEmail, IsString, MaxLength } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
   email: string;
 
   @IsString()
-  @MinLength(8)
   @MaxLength(100)
   password: string;
 }
