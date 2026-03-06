27366fe933a2d7a46a611c2e50fee83d5dac977d
Ghazan Khan <ghazankhan27@hotmail.com>
2026-03-06 22:53:26 +0500

    feat: Added email service

---
+++ src/modules/email/email-templates.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { resolve } from 'path';
+import * as ejs from 'ejs';
+
+export interface RenderOptions {
+  /** EJS options (e.g. escape, delimiter). */
+  options?: ejs.Options;
+}
+
+@Injectable()
+export class EmailTemplatesService {
+  private readonly basePath: string;
+
+  constructor(private readonly configService: ConfigService) {
+    const dir = this.configService.get<string>('TEMPLATES_DIR', 'templates');
+    this.basePath = resolve(process.cwd(), dir);
+  }
+
+  /**
+   * Render an EJS template string with the given data.
+   */
+  render(template: string, data: Record<string, unknown> = {}, opts?: RenderOptions): string {
+    return ejs.render(template, data, opts?.options);
+  }
+
+  /**
+   * Render an EJS file under the templates directory. Path is relative to TEMPLATES_DIR (default: project root / templates).
+   * Example: renderFile('emails/welcome.ejs', { name: 'Jane' })
+   */
+  async renderFile(
+    relativePath: string,
+    data: Record<string, unknown> = {},
+    opts?: RenderOptions,
+  ): Promise<string> {
+    const filePath = resolve(this.basePath, relativePath);
+    return ejs.renderFile(filePath, data, opts?.options);
+  }
+}
--- src/modules/email/email.module.ts
+++ src/modules/email/email.module.ts
@@ -1,9 +1,10 @@
 import { Module, Global } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { EmailTemplatesService } from './email-templates.service';
 
 @Global()
 @Module({
-  providers: [EmailService],
-  exports: [EmailService],
+  providers: [EmailService, EmailTemplatesService],
+  exports: [EmailService, EmailTemplatesService],
 })
 export class EmailModule {}
